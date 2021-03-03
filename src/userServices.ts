fdff93e06dfc890d4f0e22c75b47bbd297bd3a6b
mikezimm <49648086+mikezimm@users.noreply.github.com>
2021-03-02 19:00:49 -0500

    Update user services, v0.0.0.29

--- src/userServices.ts
+++ src/userServices.ts
@@ -1,8 +1,10 @@
-import { Web, IList, IItem, IItemAddResult, IWebEnsureUserResult } from "@pnp/sp/presets/all";
+import { Web, IList, IItem, IItemAddResult, IWebEnsureUserResult, PrincipalType } from "@pnp/sp/presets/all";
 
 import "@pnp/sp/webs";
 import "@pnp/sp/clientside-pages/web";
 import "@pnp/sp/site-users/web";
+
+import { PermissionKind } from '@pnp/sp/presets/all';
 
 import { IRefiners, IRefinerLayer, IItemRefiners, RefineRuleValues, RefinerStatTypes, IRefinerStats, IRefinerStatType, IUser } from './IReUsableInterfaces';
 
@@ -15,6 +17,38 @@ import { getHelpfullError } from './ErrorHandler';
 import { doesObjectExistInArray } from './arrayServices';
 import { isEqual } from '@microsoft/sp-lodash-subset';
 
+
+/***
+ *     d888b  d88888b d888888b      d8888b. d8888b. d888888b d8b   db  .o88b. d888888b d8888b.  .d8b.  db           d888888b db    db d8888b. d88888b 
+ *    88' Y8b 88'     `~~88~~'      88  `8D 88  `8D   `88'   888o  88 d8P  Y8   `88'   88  `8D d8' `8b 88           `~~88~~' `8b  d8' 88  `8D 88'     
+ *    88      88ooooo    88         88oodD' 88oobY'    88    88V8o 88 8P         88    88oodD' 88ooo88 88              88     `8bd8'  88oodD' 88ooooo 
+ *    88  ooo 88~~~~~    88         88~~~   88`8b      88    88 V8o88 8b         88    88~~~   88~~~88 88              88       88    88~~~   88~~~~~ 
+ *    88. ~8~ 88.        88         88      88 `88.   .88.   88  V888 Y8b  d8   .88.   88      88   88 88booo.         88       88    88      88.     
+ *     Y888P  Y88888P    YP         88      88   YD Y888888P VP   V8P  `Y88P' Y888888P 88      YP   YP Y88888P         YP       YP    88      Y88888P 
+ *                                                                                                                                                    
+ *                                                                                                                                                    
+ */
+
+export function getPrincipalTypeString( type: PrincipalType ) {
+  if ( type === 0 ) { return 'None'; }
+  if ( type === 1 ) { return 'User'; }
+  if ( type === 2 ) { return 'Distribution'; }
+  if ( type === 4 ) { return 'Security'; }
+  if ( type === 8 ) { return 'SharePoint'; }
+  if ( type === 15 ) { return 'All'; }
+}
+
+
+/***
+ *    d88888b d8b   db .d8888. db    db d8888b. d88888b      db    db .d8888. d88888b d8888b.      d888888b d8b   db d88888b  .d88b.  
+ *    88'     888o  88 88'  YP 88    88 88  `8D 88'          88    88 88'  YP 88'     88  `8D        `88'   888o  88 88'     .8P  Y8. 
+ *    88ooooo 88V8o 88 `8bo.   88    88 88oobY' 88ooooo      88    88 `8bo.   88ooooo 88oobY'         88    88V8o 88 88ooo   88    88 
+ *    88~~~~~ 88 V8o88   `Y8b. 88    88 88`8b   88~~~~~      88    88   `Y8b. 88~~~~~ 88`8b           88    88 V8o88 88~~~   88    88 
+ *    88.     88  V888 db   8D 88b  d88 88 `88. 88.          88b  d88 db   8D 88.     88 `88.        .88.   88  V888 88      `8b  d8' 
+ *    Y88888P VP   V8P `8888Y' ~Y8888P' 88   YD Y88888P      ~Y8888P' `8888Y' Y88888P 88   YD      Y888888P VP   V8P YP       `Y88P'  
+ *                                                                                                                                    
+ *                                                                                                                                    
+ */
 
 export async function ensureUserInfo ( webURL: string, userEmail: string ) {
 
@@ -48,26 +82,25 @@ export async function ensureUserInfo ( webURL: string, userEmail: string ) {
 
     return thisUser;
 }
+
 /***
- *    .d888b.  .d88b.  .d888b.  .d88b.          db .d888b.         db   j88D                     
- *    VP  `8D .8P  88. VP  `8D .8P  88.        o88 VP  `8D        o88  j8~88                     
- *       odD' 88  d'88    odD' 88  d'88         88    odD'         88 j8' 88                     
- *     .88'   88 d' 88  .88'   88 d' 88 C8888D  88  .88'   C8888D  88 V88888D                    
- *    j88.    `88  d8' j88.    `88  d8'         88 j88.            88     88                     
- *    888888D  `Y88P'  888888D  `Y88P'          VP 888888D         VP     VP                     
- *                                                                                               
- *                                                                                               
- *    d8888b. d888888b db    db  .d88b.  d888888b      d888888b d888888b db      d88888b .d8888. 
- *    88  `8D   `88'   88    88 .8P  Y8. `~~88~~'      `~~88~~'   `88'   88      88'     88'  YP 
- *    88oodD'    88    Y8    8P 88    88    88            88       88    88      88ooooo `8bo.   
- *    88~~~      88    `8b  d8' 88    88    88            88       88    88      88~~~~~   `Y8b. 
- *    88        .88.    `8bd8'  `8b  d8'    88            88      .88.   88booo. 88.     db   8D 
- *    88      Y888888P    YP     `Y88P'     YP            YP    Y888888P Y88888P Y88888P `8888Y' 
- *                                                                                               
- *                                                                                               
+ *     .o88b. db   db d88888b  .o88b. db   dD      d888888b d88888b      db    db .d8888. d88888b d8888b.                     
+ *    d8P  Y8 88   88 88'     d8P  Y8 88 ,8P'        `88'   88'          88    88 88'  YP 88'     88  `8D                     
+ *    8P      88ooo88 88ooooo 8P      88,8P           88    88ooo        88    88 `8bo.   88ooooo 88oobY'                     
+ *    8b      88~~~88 88~~~~~ 8b      88`8b           88    88~~~        88    88   `Y8b. 88~~~~~ 88`8b                       
+ *    Y8b  d8 88   88 88.     Y8b  d8 88 `88.        .88.   88           88b  d88 db   8D 88.     88 `88.                     
+ *     `Y88P' YP   YP Y88888P  `Y88P' YP   YD      Y888888P YP           ~Y8888P' `8888Y' Y88888P 88   YD                     
+ *                                                                                                                            
+ *                                                                                                                            
+ *    d88888b db    db d888888b .d8888. d888888b .d8888.      d888888b d8b   db       .d8b.  d8888b. d8888b.  .d8b.  db    db 
+ *    88'     `8b  d8'   `88'   88'  YP `~~88~~' 88'  YP        `88'   888o  88      d8' `8b 88  `8D 88  `8D d8' `8b `8b  d8' 
+ *    88ooooo  `8bd8'     88    `8bo.      88    `8bo.           88    88V8o 88      88ooo88 88oobY' 88oobY' 88ooo88  `8bd8'  
+ *    88~~~~~  .dPYb.     88      `Y8b.    88      `Y8b.         88    88 V8o88      88~~~88 88`8b   88`8b   88~~~88    88    
+ *    88.     .8P  Y8.   .88.   db   8D    88    db   8D        .88.   88  V888      88   88 88 `88. 88 `88. 88   88    88    
+ *    Y88888P YP    YP Y888888P `8888Y'    YP    `8888Y'      Y888888P VP   V8P      YP   YP 88   YD 88   YD YP   YP    YP    
+ *                                                                                                                            
+ *                                                                                                                            
  */
-
-
 
 export function checkIfUserExistsInArray( recentUsers : IUser[] , user: IUser ) {
 
@@ -284,11 +317,18 @@ export async function ensureTheseUsers ( theseUsers: IUser[], checkTheseUsers: I
   }
 
 
-  /**
-   * Updated function from https://github.com/pnp/pnpjs/issues/1480#issuecomment-745203843
-   * 
-  */
-  import { PermissionKind } from '@pnp/sp/presets/all';
+
+
+  /***
+ *     d888b  d88888b d888888b      db    db .d8888. d88888b d8888b.      d8888b. d88888b d8888b. .88b  d88. d888888b .d8888. .d8888. d888888b  .d88b.  d8b   db .d8888. 
+ *    88' Y8b 88'     `~~88~~'      88    88 88'  YP 88'     88  `8D      88  `8D 88'     88  `8D 88'YbdP`88   `88'   88'  YP 88'  YP   `88'   .8P  Y8. 888o  88 88'  YP 
+ *    88      88ooooo    88         88    88 `8bo.   88ooooo 88oobY'      88oodD' 88ooooo 88oobY' 88  88  88    88    `8bo.   `8bo.      88    88    88 88V8o 88 `8bo.   
+ *    88  ooo 88~~~~~    88         88    88   `Y8b. 88~~~~~ 88`8b        88~~~   88~~~~~ 88`8b   88  88  88    88      `Y8b.   `Y8b.    88    88    88 88 V8o88   `Y8b. 
+ *    88. ~8~ 88.        88         88b  d88 db   8D 88.     88 `88.      88      88.     88 `88. 88  88  88   .88.   db   8D db   8D   .88.   `8b  d8' 88  V888 db   8D 
+ *     Y888P  Y88888P    YP         ~Y8888P' `8888Y' Y88888P 88   YD      88      Y88888P 88   YD YP  YP  YP Y888888P `8888Y' `8888Y' Y888888P  `Y88P'  VP   V8P `8888Y' 
+ *                                                                                                                                                                       
+ *        Updated function from https://github.com/pnp/pnpjs/issues/1480#issuecomment-745203843                                                                                                                                                               
+ */
 
   export async function getUserPermissions( webUrl: string , supressError: boolean ) {
     let thisWeb = Web(webUrl);
@@ -318,6 +358,17 @@ export async function ensureTheseUsers ( theseUsers: IUser[], checkTheseUsers: I
     }
 
   }
+
+  /***
+ *     d888b  d88888b d888888b      .d8888. d888888b d888888b d88888b       .d8b.  d8888b. .88b  d88. d888888b d8b   db .d8888. 
+ *    88' Y8b 88'     `~~88~~'      88'  YP   `88'   `~~88~~' 88'          d8' `8b 88  `8D 88'YbdP`88   `88'   888o  88 88'  YP 
+ *    88      88ooooo    88         `8bo.      88       88    88ooooo      88ooo88 88   88 88  88  88    88    88V8o 88 `8bo.   
+ *    88  ooo 88~~~~~    88           `Y8b.    88       88    88~~~~~      88~~~88 88   88 88  88  88    88    88 V8o88   `Y8b. 
+ *    88. ~8~ 88.        88         db   8D   .88.      88    88.          88   88 88  .8D 88  88  88   .88.   88  V888 db   8D 
+ *     Y888P  Y88888P    YP         `8888Y' Y888888P    YP    Y88888P      YP   YP Y8888D' YP  YP  YP Y888888P VP   V8P `8888Y' 
+ *                                                                                                                              
+ *                                                                                                                              
+ */
 
   export async function getSiteAdmins( webUrl: string , supressError: boolean ) {
     let thisWeb = Web(webUrl);
@@ -352,33 +403,3 @@ export async function ensureTheseUsers ( theseUsers: IUser[], checkTheseUsers: I
   }
 }
 
-
-  export async function getSiteAdminsOrigMustBeSiteAdmin( webUrl: string , supressError: boolean ) {
-    
-    let thisWeb = Web(webUrl);
-
-    let errMessage = null;
-    let adminFilter = "IsSiteAdmin eq true";
-    //This works in React PnpJS Tester:  sp.web.siteUsers.filter("IsSiteAdmin eq true")
-    try {
-        const users = thisWeb.siteUsers;
-        let returnUsers = await users.filter(adminFilter).get();
-        console.log('getSiteAdmins: users', returnUsers );
-
-        return { users: returnUsers, errMessage: errMessage }  ;
-
-    } catch (e) {
-        errMessage = getHelpfullError(e, true, true);
-
-        if ( supressError === true && errMessage.indexOf('Save Conflict') === 0 ) {
-          //Do nothting
-        } else {
-          alert( errMessage );
-        }
-
-        console.log( 'getSiteAdmins', errMessage );
-
-        return { users: [], errMessage: errMessage }  ;
-    }
-
-}
